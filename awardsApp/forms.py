1f4ae25bf2ce6431629324071fd25693d3c85f90
Demarillac Izere <izered3@gmail.com>
2021-01-17 12:09:27 +0200

    added forms

---
+++ awardsApp/forms.py
@@ -0,0 +1,23 @@
+
+from django import forms
+from .models import Profile, Project
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class NewProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user','email']
