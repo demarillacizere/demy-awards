a27852d8d1030f870bc3c695f9c67afed63fa5b9
Demarillac Izere <izered3@gmail.com>
2021-01-17 19:37:33 +0200

    added a serializer file

---
+++ awardsApp/serializer.py
@@ -0,0 +1,13 @@
+
+from rest_framework import serializers
+from .models import Profile,Project
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Profile
+        fields=('bio','email','profile_pic','user')
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Project
+        fields=('webimage','name','description','link','profile','no_of_ratings')
